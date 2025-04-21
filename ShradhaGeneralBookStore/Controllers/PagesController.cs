76817e15fc5c7a2a26bcbbf631d199e78b7dc287
wisamahmed851 <168702325+wisamahmed851@users.noreply.github.com>
2025-04-22 02:28:01 +0500

    jjjjj

--- ShradhaGeneralBookStore/Controllers/PagesController.cs
+++ ShradhaGeneralBookStore/Controllers/PagesController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShradhaGeneralBookStore.Datas;
 
 namespace ShradhaGeneralBookStore.Controllers
 {
     public class PagesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public PagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult AboutUs()
         {
             return View();
@@ -14,10 +22,32 @@ namespace ShradhaGeneralBookStore.Controllers
             return View();
         }
 
-        public IActionResult Faqs()
+        public async Task<IActionResult> Faqs()
         {
+            var categories = await _context.FaqCategories.Where(x => x.IsActive).ToListAsync();
+            var firstCategoryId = categories.FirstOrDefault()?.Id;
+
+            var faqs = await _context.Faqs
+                .Where(f => f.CategoryId == firstCategoryId && f.IsActive)
+                .ToListAsync();
+
+            ViewBag.Categories = categories;
+            ViewBag.Faqs = faqs;
+            ViewBag.SelectedCategoryId = firstCategoryId;
+
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFaqsByCategory(int categoryId)
+        {
+            var faqs = await _context.Faqs
+                .Where(f => f.CategoryId == categoryId && f.IsActive)
+                .ToListAsync();
+
+            return PartialView("_FaqListPartial", faqs);
+        }
+
 
         public IActionResult Contact()
         {
