6321126201158807abb49b8efc8191c051805bc3
Wisam851 <wisam@bidecsol.com>
2025-04-29 18:57:55 +0500

    kadsjfhasdfkj

--- ShradhaGeneralBookStore/Controllers/ProductsApiController.cs
+++ ShradhaGeneralBookStore/Controllers/ProductsApiController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShradhaGeneralBookStore.Datas;
+using ShradhaGeneralBookStore.Dtos;
 
 namespace ShradhaGeneralBookStore.Controllers
 {
@@ -19,7 +20,17 @@ namespace ShradhaGeneralBookStore.Controllers
         public async Task<IActionResult> GetAllActiveProducts()
         {
             var products = await _context.Product
+                .Include(p => p.Category)
+                .Include(p => p.Subcategory)
                 .Where(p => p.status == "Active")
+                .Select(p => new ProductDto
+                {
+                    Name = p.Name,
+
+                    CategoryName = p.Category.Name,
+                    SubcategoryName = p.Subcategory.Name,
+                    Price = p.Price
+                })
                 .ToListAsync();
 
             return Ok(products);
---
+++ ShradhaGeneralBookStore/Dtos/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShradhaGeneralBookStore.Dtos
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public string SubcategoryName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
