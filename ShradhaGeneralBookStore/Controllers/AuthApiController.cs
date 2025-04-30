d0fdd8b9c9cf39e43879997f4fbffdd7062e90f3
Wisam851 <wisam@bidecsol.com>
2025-04-30 23:40:40 +0500

    awdawdaawd

---
+++ ShradhaGeneralBookStore/Controllers/AuthApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShradhaGeneralBookStore.Controllers
+{
+    public class AuthApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
